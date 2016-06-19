c1868de12c2a0887286a375e22908a9d31249a33
Berta Devant <bertadeva@gmail.com>
2016-06-19 13:37:26 -0400

    fixing it again

--- app.js
+++ app.js
@@ -314,12 +314,12 @@ function receivedMessage(event) {
             //user.facts.push(messageText);
             console.log("check if pool "+checkIfPool(messageText));
             stateOftheApp.catPool = 4;
-            askKeyquestions(event);
+            //askKeyquestions(event);
           }
         } else {
           sendMessage(senderID,getReaction());
           //user.facts.push(messageText);
-          //stateOftheApp.catPool = checkIfPool(messageText);
+          stateOftheApp.catPool = 4;
         }
         break;
       case 3:
